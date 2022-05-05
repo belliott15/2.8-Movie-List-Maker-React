8f7e933af70fb386bccdd69023d60fb70a48a819
Beau Elliott <belliott1516@gmail.com>
2022-05-05 16:44:38 -0700

    had to correct the last fix

--- src/Movie.js
+++ src/Movie.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Movie({ movie, handleDeleteMovie }) {
   return (
     <div>
-      {movie.title || movie.year || movie.director && <div className='movie-card' style={{ background: movie.color, color: 'black' }} onClick={() => handleDeleteMovie(movie.title)}>
+      {movie.title || movie.year || movie.director ? <div className='movie-card' style={{ background: movie.color, color: 'black' }} onClick={() => handleDeleteMovie(movie.title)}>
         <h2>{movie.title}</h2>
         <h3>{movie.year}</h3>
         {movie.director && <h4>Directed by:{movie.director}</h4>}
-      </div> }
+      </div> : '' }
     </div>
   );
 }
