5691280ecd36e13de55fcb7eef1a4350c75ad6a0
Beau Elliott <belliott1516@gmail.com>
2022-05-05 16:27:38 -0700

    added styling to movie posters and added more of a standard set

--- src/App.js
+++ src/App.js
@@ -8,7 +8,39 @@ import './App.css';
 function App() {
 
   //Tracking state
-  const [allMovies, setAllMovies] = useState([]);
+  const [allMovies, setAllMovies] = useState([
+    { 
+      title: '2001: A Space Odyssey',
+      year: 1968,
+      director: 'Stanley Kubrick',
+      color: '#e76f51'
+    },
+    { 
+      title: 'The Godfather',
+      year: 1972,
+      director: 'Francis Coppola',
+      color: '#2a9d8f'
+    },
+    { 
+      title: 'Citizen Kane',
+      year: 1941,
+      director: 'David Fincher',
+      color: '#e9c46a'
+    },
+    { 
+      title: 'Jeanne Dielman ...',
+      year: 1975,
+      director: 'Chantal Akerman',
+      color: '#e76f51'
+    },
+    { 
+      title: 'Raiders of the Lost Ark',
+      year: 1981,
+      director: 'Steven Spielberg',
+      color: '#f4a261'
+    },
+    
+  ]);
   const [filteredMovies, setFilteredMovies] = useState('');
   const [movieTitle, setMovieTitle] = useState('');
   const [movieFormColor, setMovieFormColor] = useState('#2a9d8f');
