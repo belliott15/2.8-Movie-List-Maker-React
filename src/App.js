bad2aff3d404b21395e61e8ded151dd04fbd855a
Beau Elliott <belliott1516@gmail.com>
2022-05-05 09:57:48 -0700

    added framework and wireframing for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  //Tracking state
+  const [allMovies, setAllMovies] = useState();
+  const [filteredMovies, setFilteredMovies] = useState();
+  const [movieTitle, setMovieTitle] = useState();
+  const [movieFormColor, setMovieFormColor] = useState();
+  const [movieFormYear, setMovieFormYear] = useState();
+  const [movieFormDirector, setMovieFormDirector] = useState();
+  
+  //create delete function to delete movies on click
+  function handleDeleteMovie(){
+
+  }
+  // use Effect to only activate when state changes
+  // useEffect();
+  //created filter function so search by movie title
+  function handleMovieFilter(){
+
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Movie Maker
       </header>
+      <main>
+        <form>
+          <h1>Title</h1>
+          <input />
+          <h1>Year</h1>
+          <input />
+          <h1>Director</h1>
+          <input />
+          <h1>Background Color</h1>
+          <select>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+            <option value='crimson'>Red</option>
+          </select>
+          <button>Submit</button>
+        </form>
+        <h2>Title</h2>
+        <h3>year</h3>
+        <h5>Directed By:</h5>
+        
+        <h2>Title</h2>
+        <h3>year</h3>
+        <h5>Directed By:</h5>
+      </main>
     </div>
   );
 }
