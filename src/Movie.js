96bafb3d32b89834755c08669a1f35db984d47f1
Beau Elliott <belliott1516@gmail.com>
2022-05-05 17:07:52 -0700

    added space in director

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ export default function Movie({ movie, handleDeleteMovie }) {
       {movie.title || movie.year || movie.director ? <div className='movie-card' style={{ background: movie.color, color: 'black' }} onClick={() => handleDeleteMovie(movie.title)}>
         <h2>{movie.title}</h2>
         <h3>{movie.year}</h3>
-        {movie.director && <h4>Directed by:{movie.director}</h4>}
+        {movie.director && <h4>Directed by: {movie.director}</h4>}
       </div> : '' }
     </div>
   );
